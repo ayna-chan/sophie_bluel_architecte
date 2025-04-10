5fc0a1984f5f1502e022a45d32cdf3b242c491e8
Tiffany <marintiffany3@gmail.com>
2025-04-10 18:33:20 +0200

    Authentification utilisateur

---
+++ FrontEnd/login.js
@@ -0,0 +1,51 @@
+// fetch("http://localhost:5678/api/users/login", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         email: "string",
+//         password: "string"
+//     })
+// })
+
+// .then(response => response.json())
+// .then(data => {
+//     console.log("Réponse serveur :", data);
+//     // ici tu peux ajouter la redirection si tout s'est bien passé
+// })
+// .catch(error => {
+//     console.error("Erreur lors de la requête :", error);
+// });
+
+
+const formulaireLogin = document.querySelector(".login-form");
+
+formulaireLogin.addEventListener("submit", async function (event) {
+    //désactivation du comportement par défaut du formulaire
+    event.preventDefault();
+
+    const email = event.target.querySelector("[name=email]").value;
+    const password = event.target.querySelector("[name=password]").value;
+
+    const utilisateur = { email, password };
+
+    
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(utilisateur)
+    });
+
+    if (reponse.ok) {
+        const data = await reponse.json();
+        console.log("Connexion réussie:", data);
+        localStorage.setItem("token", data.token);
+        window.location.href = "index.html"; // redirection vers la page d'accueil
+
+    } else {
+        // affichage d'une alerte en cas d'erreur
+        alert("Erreur dans l'identifiant ou le mot de passe");
+    }
+});
+
